cf83092c6d6340800bf016bd01105d03ce9425f3
AGD IT <AGD@farazis-iMac.local>
2015-05-02 15:29:38 +0600

    initial commit

---
+++ BinaryTree/BinaryTree.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace BinaryTree
+{
+	public class BinaryTree
+	{	
+		private Node root;
+		public BinaryTree(){
+			root = null;
+		}
+
+		public virtual void Clear(){
+			root = null;
+		}
+
+
+		public Node Root{
+			get{ return root;}
+			set{ root = value;}
+
+		}
+	}
+}
+
---
+++ BinaryTree/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace BinaryTree
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Hello World!");
+			BinaryTree btree = new BinaryTree();
+			btree.Root = new Node (1);
+			btree.Root.Value = 3;
+			Console.WriteLine (btree.Root.Value);
+			btree.Root.Left = new Node (2);
+			btree.Root.Right = new Node (3);
+
+
+			btree.Root.Left.Left = new Node (4);
+			btree.Root.Right.Right = new Node (5);
+
+		}
+	}
+}
